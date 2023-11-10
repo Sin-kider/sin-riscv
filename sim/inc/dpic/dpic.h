c8cf27f72cdf6855567c7f8013107f54d9af46dc
Sin_kider <2210486110@qq.com>
2023-11-10 20:22:53 +0800

    🦄 refactor: 删除残留代码

--- sim/inc/dpic/dpic.h
+++ sim/inc/dpic/dpic.h
@@ -1,7 +1,4 @@
 #ifndef __DPIC_H__
 #define __DPIC_H__
 
-#define HEX_CODE_LEN 340
-#define CODE_LEN HEX_CODE_LEN
-
 #endif
--- sim/inc/utils/utils.h
+++ sim/inc/utils/utils.h
@@ -1,8 +1,4 @@
 #ifndef __UTILS__
 #define __UTILS__
 
-#include <cstdint>
-void init_disasm(const char *triple);
-void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
-
 #endif
